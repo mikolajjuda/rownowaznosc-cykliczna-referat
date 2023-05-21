0020dde3e43961211dec12bc5079ee5853a4c5b1
mikolajjuda <mikolaj.juda@gmail.com>
2023-05-21 10:29:29 +0200

    changed code listings to minted

--- referat/naive.py
+++ referat/naive.py
@@ -2,9 +2,9 @@ def rownowazne_cyklicznie(a: list, b: list) -> bool:
     if len(a) != len(b):
         return False
     n = len(a)
-    for c in range(n):
+    for l in range(n):
         for k in range(n):
-            if a[(c + k) % n] != b[k]:
+            if a[(l + k) % n] != b[k]:
                 break
         else:
             return True
